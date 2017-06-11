38814afa08ee33dd35d3fda0c719618c4d9f8641
sw1234567 <schoolteamwork@gmail.com>
2017-06-10 23:54:26 -0700

    Update Spawner.java

--- src/project/main/Spawner.java
+++ src/project/main/Spawner.java
@@ -47,25 +47,42 @@ public class Spawner {
 	public void tick(){	
 		if (numberOfEnemies == 0){
 			hud.setLevel(hud.getLevel() + 1);
-			if (hud.getLevel() < 15){
-				for (int i = 0; i < 2 * hud.getLevel(); i++){
+			if (hud.getLevel() % 49 == 0){
+				for (int i = 0; i < 10 * hud.getLevel(); i++){
 					int [] coord = enemyCoordinates();
 					handler.addObject(new Zombie(coord[0], coord[1], handler, hud));
 					numberOfEnemies ++;
 				}
 			}
-			if (hud.getLevel() >= 15 && hud.getLevel() <= 30){
-				for (int i = 0; i < 2 * (hud.getLevel() - 15); i++){
-					int [] coord = enemyCoordinates();
-					handler.addObject(new ZombieThrower(coord[0], coord[1], handler, hud));
-					numberOfEnemies ++;
+			else {
+				if (hud.getLevel() < 15){
+					for (int i = 0; i < 2 * hud.getLevel(); i++){
+						int [] coord = enemyCoordinates();
+						handler.addObject(new Zombie(coord[0], coord[1], handler, hud));
+						numberOfEnemies ++;
+					}
 				}
-			}
-			if (hud.getLevel() >= 1){
-				for (int i = 0; i < 10 * (hud.getLevel() - 1); i++){
-					int [] coord = enemyCoordinates();
-					handler.addObject(new ZombieKnight(coord[0], coord[1], handler, hud));
-					numberOfEnemies ++;
+				if (hud.getLevel() >= 15){
+					for (int i = 0; i < 3 * (hud.getLevel() - 15); i++){
+						int [] coord = enemyCoordinates();
+						handler.addObject(new ZombieThrower(coord[0], coord[1], handler, hud));
+						numberOfEnemies ++;
+					}
+				}
+				if (hud.getLevel() >= 30){
+					for (int i = 0; i < 4 * (hud.getLevel() - 30); i++){
+						int [] coord = enemyCoordinates();
+						handler.addObject(new ZombieKnight(coord[0], coord[1], handler, hud));
+						numberOfEnemies ++;
+					}
+				}
+				if (hud.getLevel() % 50 == 0){
+					for (int i = 0; i < 600 * hud.getLevel() / 50; i++){
+						int [] coord = enemyCoordinates();
+						handler.addObject(new ZombieKnight(coord[0], coord[1], handler, hud));
+						handler.addObject(new Zombie(coord[0], coord[1], handler, hud));
+						numberOfEnemies += 2;
+					}
 				}
 			}
 		}
