9118ca631eabd7af5bdf678e950972370fb908a1
sw1234567 <schoolteamwork@gmail.com>
2017-06-11 14:18:43 -0700

    Create ZombieKnight.java

--- src/project/main/ZombieKnight.java
+++ src/project/main/ZombieKnight.java
@@ -237,7 +237,7 @@ public class ZombieKnight extends Enemy {
 	}
 
 	public void render(Graphics g) {
-		Image img = new ImageIcon(this.getClass().getResource("/ZombieKnight.png")).getImage();
+		Image img = new ImageIcon(this.getClass().getResource("/zombieKnight.png")).getImage();
 		//Camera c = Game.camera;
 		updateWindowCoordinates();
 		//g.drawImage(img, (int)(x - c.X + Game.WIDTH / 2), (int)(y - c.Y + Game.HEIGHT / 2), (int)(x + 32 - c.X + Game.WIDTH / 2), (int)(y + 32 - c.Y + Game.HEIGHT / 2), 0, 0, 32, 32, null);
